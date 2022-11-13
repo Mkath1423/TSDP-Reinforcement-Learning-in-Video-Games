a3209e1176a2fe03c7d091b64e8e1e334236a2dd
shiqui <44322197+shiqui@users.noreply.github.com>
2022-11-12 21:45:20 -0500

    Add update_states() and basic collision

--- game/level.py
+++ game/level.py
@@ -5,9 +5,6 @@ from bullet import Bullet
 
 class Level():
     def __init__(self):
-        self.agents_states = {}
-        self.bullets_states = {}
-
         self.agents = GameObjectGroup("agents")
         self.bullets = GameObjectGroup("bullets")
         self.render_on = False
@@ -15,30 +12,72 @@ class Level():
     def toggle_render(self, render_on):
         self.render_on = render_on
 
-    def add_agent(self, name, damage, health, state, color=(255,0,0)):
-        new_agent = Agent(name, damage, health, state, color)
-        self.agents_states[new_agent.get_id] = state
-        if self.render_on:
-            self.agents.add(new_agent)
+    def add_agent(self, name, state, color=(255,0,0)):
+        self.agents.add(Agent(name, state, color))
 
     def add_bullet(self, name, state):
-        new_bullet = Bullet(name, state)
-        self.bullets_states[new_bullet.get_id] = state
-        if self.render_on:
-            self.bullets.add(new_bullet)
+        self.bullets.add(Bullet(name, state))
 
-    def update_states(self, new_agents_states):
-        self.agents_states = new_agents_states
+    def collision(self):
+        agent_states = self.agents.get_state()
+        bullet_states = self.bullets.get_state()
 
-        for a in self.agents:
-            a.update_state(new_agents_states[a.get_id])
+        for a in agent_states:
+            a_pos = agent_states[a]['position']
+            for b in bullet_states:
+                if bullet_states[b] == None:
+                    continue
+                b_pos = bullet_states[b]['position']
+                if (a_pos[0] < b_pos[0] + 5 and a_pos[0] + 50 > b_pos[0] and
+                    a_pos[1] < b_pos[1] + 5 and a_pos[1] + 50 > b_pos[1]):
 
-        for b in self.bullets:
-            pos = self.bullets_states[b.get_id]['position']
-            v = self.bullets_states[b.get_id]['velocity']
-            new_pos = (pos[0]+v[0], pos[1]+v[1])
-            self.bullets_states[b.get_id]['position'] = new_pos
-            b.update_state(self.bullets_states[b.get_id])
+                    agent_states[a]['hp'] -= 10
+                    bullet_states[b] = None
+
+
+        self.agents.update_state(agent_states)
+        self.bullets.update_state(bullet_states)
+
+
+    def update_states(self):
+        move_set = ['w','a','s','d', 'rest']
+
+        agent_states = self.agents.get_state()
+        agent_moves = self.agents.get_moves([self.agents, self.bullets])
+
+        for i in agent_states:
+            move = move_set[agent_moves[i]]
+            pos = agent_states[i]['position']
+            new_pos = pos
+
+            if(move == 'w'):
+                new_pos = (pos[0], pos[1]-5)
+            elif(move == 'a'):
+                new_pos = (pos[0]-5, pos[1])
+            elif(move == 's'):
+                new_pos = (pos[0], pos[1]+5)
+            elif(move == 'd'):
+                new_pos = (pos[0]+5, pos[1])
+            elif(move == 'rest'):
+                new_pos = pos
+            # TODO new position sanity check
+            # collision
+            # game config file?
+
+            agent_states[i]['position'] = new_pos
+
+        self.agents.update_state(agent_states)
+            
+
+        bullet_states = self.bullets.get_state()
+        for i in bullet_states:
+            pos = bullet_states[i]['position']
+            vel = bullet_states[i]['velocity']
+            new_pos = (pos[0]+vel[0], pos[1]+vel[1])
+            bullet_states[i]['position'] = new_pos
+        
+        self.bullets.update_state(bullet_states)
+    
 
     def render(self, screen):
         assert(self.render_on)
@@ -46,3 +85,6 @@ class Level():
         self.agents.draw(screen)
         self.bullets.update()
         self.bullets.draw(screen)
+
+    def __repr__(self):
+        return f'agents:{self.agents_states}, bullets:{self.bullets_states}'
